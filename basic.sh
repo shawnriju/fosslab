bfbf5eb6e64fa44d79af19cb85e413e90515916b
shawnriju <shawnriju@gmail.com>
2018-02-27 16:04:15 +0530

    GG

---
+++ basic.sh
@@ -0,0 +1,8 @@
+
+echo "Your username : $(echo $USER)"
+echo "Currently logged on users:" $(who)
+echo " home directory is " $PATH
+echo " os is" $(uname -a)
+ echo "shell used is" $SHELL
+echo "working directory is " $PWD
+echo "WHO AM I" $(whoami)
